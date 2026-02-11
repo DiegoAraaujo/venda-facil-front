afc73eccb9e0c83fd9f086d1c9098300cfd3ae76
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-11 07:47:14 -0300

    feat(settings): add placeholder for Settings page under construction

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,9 @@
+const Settings = () => {
+  return (
+    <div className="text-gray-500 flex-1 flex items-center justify-center text-2xl p-4 md:p-8">
+      🚧 Página em construção
+    </div>
+  );
+};
+
+export default Settings;
